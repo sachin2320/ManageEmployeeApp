03f7bc9adf30a1a948cbd71786e50d97fdeb80d8
sachin gupta <sachin2320@gmail.com>
2021-11-13 20:56:31 +0530

    push changes

---
+++ ManageEmployees/Models/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ManageEmployees.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal? Salary { get; set; }
+        public string City
+        {
+            get; set;
+        }
+    }
+}
